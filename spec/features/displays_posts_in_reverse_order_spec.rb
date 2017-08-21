7580238ef9ad16c5ea9d949e37935d011c695aa9
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-21 18:27:29 +0100

    Added test for displaying posts in reverse order and it passes

--- spec/features/displays_posts_in_reverse_order_spec.rb
+++ spec/features/displays_posts_in_reverse_order_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
-require 'web_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "posts are displayed in reverse order" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    create_new_post
+    create_new_post(message: "Aks says Makers Academy week 7 Acebook challenge is going crazy")
+    create_new_post(message: "Eoin says I love to code everyday with my online friends")
+    expect(page).to have_content(page_content(Post.all.reverse))
   end
+end
+
+def create_new_post(message: "Hello, world")
+  visit "/posts"
+  click_link "New post"
+  fill_in "Message", with: message
+  click_button "Submit"
+end
+
+def page_content(posts)
+  page_content = ""
+  posts.each do |post|
+    page_content += "User Name" + " " + post.message + " "
+  end
+  page_content
 end
--- spec/features/web_helper.rb
+++
@@ -1,9 +0,0 @@
-module NewPost
-def make_a
-visit "/posts"
-click_link "New post"
-fill_in "Message", with: "Hello, world!"
-click_button "Submit"
-end
-
-end
