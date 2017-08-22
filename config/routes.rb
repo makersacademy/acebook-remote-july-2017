751ddae651bb45f0dc06b51b35f2c3123f4f2045
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-22 16:14:52 +0100

    groups can be created and they have their own pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'     
+  root 'welcome#index'
   resources :posts do
     resources :comments
   end
+
+  resources :groups
 end
---
+++ spec/features/viewing_groups_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Groups", type: :feature do
+  scenario "Can create a group" do
+    visit "/groups/new"
+    fill_in "Title", with: "Groupy name"
+    fill_in "Description", with: "Group description"
+    click_button "Create"
+    expect(page).to have_content("Groupy name")
+    expect(page).to have_content("Group description")
+  end
+end
