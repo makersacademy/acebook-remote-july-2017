6e9c058ac3eb17075d913805523c0cbfa24cc485
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-22 10:39:14 +0100

    fixed incorrect file naming leading to feature test bug

--- spec/features/users_can_post_comments_spec.rb
+++ spec/features/users_can_post_comments_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 RSpec.feature "Comments", type: :feature do
   scenario "Can submit comments on a post" do
     visit "/posts"
-    click_link "Comments"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "Comment"
     fill_in "Text", with: "This post sucks"
-    click_button "Create comment"
+    click_button "Create"
     expect(page).to have_content("This post sucks")
   end
 end
