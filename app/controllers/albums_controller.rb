4299295862d537187bac1229b7f28cc8df8187bd
paulmillen <pwmillen@gmail.com>
2017-08-22 15:50:39 +0100

    added title to Album#show

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,13 +4,18 @@ class AlbumsController < ApplicationController
   end
 
   def create
-    @album = Album.new
+    @album = Album.new(album_params)
     @album.save
     redirect_to @album
   end
 
   def show
     @album = Album.find(params[:id])
+  end
+
+  private
+  def album_params
+    params.require(:album).permit(:title, :text)
   end
 
 end
--- spec/features/create_album_spec.rb
+++ spec/features/create_album_spec.rb
@@ -4,4 +4,11 @@ feature 'Album' do
     click_button 'Save Album'
     expect(Album.count).to eq 1
   end
+
+  it 'has a title' do
+    visit 'albums/new'
+    fill_in 'Title', with: "Test"
+    click_button 'Save Album'
+    expect(page).to have_content "Test"
+  end
 end
