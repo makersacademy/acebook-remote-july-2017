b878c571f10c1de26f6b28405cb957588dd8fb47
Cong Cong Bo <cong@hippocampus.ai>
2017-08-22 16:29:55 +0100

    only signed in user can post and see posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'     
+  root 'welcome#index'
   resources :posts do
     resources :comments
   end
