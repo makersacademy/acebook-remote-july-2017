37e69f29fec246a6a9c919f96f7deb6904200efa
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-22 14:43:12 +0100

    Changed feature test to use nth child of list

--- spec/features/displays_posts_in_reverse_order_spec.rb
+++ spec/features/displays_posts_in_reverse_order_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "posts are displayed in reverse order with message and timestamp" do
     create_new_post
-    create_new_post(message: "Aks says Makers Academy week 7 Acebook challenge is going crazy")
-    create_new_post(message: "Eoin says I love to code everyday with my online friends")
-    expect(page).to have_content(page_content(Post.all.reverse))
+    create_new_post(message: "There is not enough time to do all the nothing we want to do")
+    create_new_post(message: "Oooh! What does THAT button do?")
+    expect(page.find('.posts-index-posts li:first-child')).to have_content("Oooh! What does THAT button do?")
+    expect(page.find('.posts-index-posts li:first-child')).to have_content(Post.last.created_at.localtime.strftime('%d-%b-%Y %I:%M:%S %p'))
   end
 end
 
@@ -14,12 +15,4 @@ def create_new_post(message: "Hello, world")
   click_link "New post"
   fill_in "Message", with: message
   click_button "Submit"
-end
-
-def page_content(posts)
-  page_content = ""
-  posts.each do |post|
-    page_content += "User Name" + " " + post.message + " " + post.created_at.localtime.strftime('%d-%b-%Y %I:%M:%S %p') + " "
-  end
-  page_content
 end
