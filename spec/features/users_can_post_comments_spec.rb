6d1adc150befcdb53b1be7bb0731957f6fce39a4
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-22 12:28:40 +0100

    added feature test for non-valid comment errors

--- spec/features/users_can_post_comments_spec.rb
+++ spec/features/users_can_post_comments_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.feature "Comments", type: :feature do
   scenario "Can submit comments on a post" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+    make_a_post
     click_link "Comment"
     fill_in "Text", with: "This post sucks"
     click_button "Create"
     expect(page).to have_content("This post sucks")
+  end
+
+  scenario "Errors are displayed when a comment does not validate" do
+    make_a_post
+    click_link "Comment"
+    click_button "Create"
+    expect(page).to have_content("2 errors prevented this comment from being saved")
+    expect(page).to have_content("Text can't be blank\nText is too short (minimum is 2 characters)")
   end
 end
