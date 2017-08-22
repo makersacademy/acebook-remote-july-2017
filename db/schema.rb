25e4ee601be7743b5a839f99e32d163db0525c73
Phil Lang <plangphoto@gmail.com>
2017-08-22 14:59:03 +0100

    bringing in nav styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170822101859) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +29,6 @@ ActiveRecord::Schema.define(version: 20170822101859) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,7 +46,5 @@ ActiveRecord::Schema.define(version: 20170822101859) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "comments", "posts"
-
 end
