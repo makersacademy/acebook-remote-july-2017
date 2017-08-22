73a3990d49325075a57a13a77bb86c2be4a555f1
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-22 11:45:38 +0100

    Added timestamp to message, styling pending

--- spec/features/displays_posts_in_reverse_order_spec.rb
+++ spec/features/displays_posts_in_reverse_order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "posts are displayed in reverse order" do
+  scenario "posts are displayed in reverse order with message and timestamp" do
     create_new_post
     create_new_post(message: "Aks says Makers Academy week 7 Acebook challenge is going crazy")
     create_new_post(message: "Eoin says I love to code everyday with my online friends")
@@ -19,7 +19,7 @@ end
 def page_content(posts)
   page_content = ""
   posts.each do |post|
-    page_content += "User Name" + " " + post.message + " "
+    page_content += "User Name" + " " + post.message + " " + post.created_at.strftime('%d-%b-%Y %I:%M:%S %p') + " "
   end
   page_content
 end
