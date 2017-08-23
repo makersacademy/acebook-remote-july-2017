f584e621e8cb80d8cd55b4ee5be57e496638c91d
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-23 09:30:35 +0100

    Requested change made to group_controller, from Group.new to .create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,12 +2,12 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
-  
+
   def new
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = Group.create(group_params)
 
     @group.save
     redirect_to @group
