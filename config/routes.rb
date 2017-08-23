f757916fc828ad2afa688e71f1fb32097b16e978
Cong Cong Bo <cong@hippocampus.ai>
2017-08-23 16:30:27 +0100

    Added routing and controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
 
-  devise_for :users  
+  devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'     
+  root 'welcome#index'
   resources :posts do
     resources :comments
   end
-  
+
   get 'albums/new'
   root 'albums#new'
   resources :albums
   resources :groups
+
+  get 'users/show'
+  root 'users#show'
+  resources :users
+
 end
