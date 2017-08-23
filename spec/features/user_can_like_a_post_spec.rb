0888329e0d3df49569f22c07fc6f9f768d160e9d
BDCraven <bencraven@hotmail.com>
2017-08-23 16:22:07 +0100

    Change like from button to link and brought it to posts/index page

--- spec/features/user_can_like_a_post_spec.rb
+++ spec/features/user_can_like_a_post_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.feature "Likes", type: :feature do
   scenario "Can like a post" do
     make_a_post
-    save_and_open_page
     click_link "Like"
-    save_and_open_page
-    expect(page.find('.posts-index-posts li:first-child')).to have_content("1")
+    expect(page.find('.posts-index-posts li:first-child .posts-index-post-likes')).to have_content("1")
   end
 end
