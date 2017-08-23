79cb828086c85ae93489a1042a5585dca2669232
Cong Cong Bo <cong@hippocampus.ai>
2017-08-23 13:23:07 +0100

    Removed unnecessary tests, removed sign out as unnecessary

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     sign_up
-    click_link('Logout')
-    sign_in
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
--- spec/features/users_can_post_comments_spec.rb
+++ spec/features/users_can_post_comments_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.feature "Comments", type: :feature do
   scenario "Can submit comments on a post" do
     sign_up
-    click_link('Logout')
-    sign_in
     make_a_post
     click_link "Comment"
     fill_in "Text", with: "This post sucks"
@@ -14,8 +12,6 @@ RSpec.feature "Comments", type: :feature do
 
   scenario "Errors are displayed when a comment does not validate" do
     sign_up
-    click_link('Logout')
-    sign_in
     make_a_post
     click_link "Comment"
     click_button "Create"
