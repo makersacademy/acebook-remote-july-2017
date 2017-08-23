0f3607a83fc703cc3aee4732907207870c50ac05
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-23 16:46:36 +0100

    Removed redundant spec files

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   devise_for :users  
   get 'welcome/index'
 
-  root 'welcome#index'     
+  root 'welcome#index'
   resources :posts do
     resources :comments
   end
-  
+
   get 'albums/new'
   root 'albums#new'
   resources :albums
