41ce2360d4e3cc749d4c334c623656ae3e0fdf1f
paulmillen <pwmillen@gmail.com>
2017-08-25 12:57:18 +0100

    Added user and posts relationship, added user's name to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    params.require(:post).permit(:user_id, :message)
   end
 end
