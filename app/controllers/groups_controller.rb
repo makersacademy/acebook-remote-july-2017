cfa4095b83159ffcf64382773613c737721f9424
Costas Kiteou <ckiteou22@gmail.com>
2017-09-05 15:49:11 +0100

    Updated README

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,20 @@
 class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
+    @user_groups = UserGroup.find(params[:id])
   end
 
   def new
   end
 
   def create
+    user = User.find(current_user.id)
+    p user
     @group = Group.create(group_params)
-
     @group.save
+
+    @user_groups = UserGroup.create(group_id: @group.id, user_id: user.id)
+    @user_groups.save
     redirect_to @group
   end
 
